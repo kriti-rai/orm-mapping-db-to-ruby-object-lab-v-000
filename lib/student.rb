52b9644c4ebd7866127d0b011b2e9f7dc0138ddb
Kriti Rai <kriti.rai@gmail.com>
2018-04-25 05:47:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,8 @@ class Student
     DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?", x)
   end
   def self.first_student_in_grade_10
-    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1")
+    row = DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1")
+    self.new_from_db(row)
   end
 
 
